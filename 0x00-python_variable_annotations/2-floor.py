1afb12d15f0f8bd89ff47e440b7fc89d2ccd896b
Muhadev <muhammedfayemi@gmail.com>
2024-06-06 20:39:41 +0100

    task is added

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""
+Module 2-floor
+Contains a function to return the floor of a float
+"""
+
+import math
+
+
+def floor(n: float) -> int:
+    """
+    Return the floor of a float number.
+
+    Args:
+    n (float): The float number to be floored.
+
+    Returns:
+    int: The floor of the float number.
+    """
+    return math.floor(n)
---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""
+Module 3-to_str
+Contains a function to convert a float to its string representation
+"""
+
+
+def to_str(n: float) -> str:
+    """
+    Return the string representation of a float number.
+
+    Args:
+    n (float): The float number to be converted to string.
+
+    Returns:
+    str: The string representation of the float number.
+    """
+    return str(n)
