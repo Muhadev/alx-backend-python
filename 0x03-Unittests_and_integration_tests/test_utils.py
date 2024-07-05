5455886af98c4e73bf0de71996c9cf2e678453ab
Muhadev <muhammedfayemi@gmail.com>
2024-07-05 21:19:40 +0100

    test_utils.py is added

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -25,16 +25,16 @@ class TestAccessNestedMap(unittest.TestCase):
         self.assertEqual(access_nested_map(nested_map, path), expected)
 
     @parameterized.expand([
-        ({"a": 1}, ("b",)),
-        ({"a": {"b": 2}}, ("a", "c")),
-        ({"a": {"b": 2}}, ("c",))
+        ({}, ("a",), "a"),
+        ({"a": 1}, ("a", "b"), "b")
     ])
-    def test_access_nested_map_exception(self, nested_map, path):
+    def test_access_nested_map_exception(self, nested_map, path, expected_exception_message):
         """
         Test that access_nested_map raises a KeyError for invalid paths.
         """
-        with self.assertRaises(KeyError):
+        with self.assertRaises(KeyError) as cm:
             access_nested_map(nested_map, path)
+        self.assertEqual(str(cm.exception), expected_exception_message)
 
 
 if __name__ == "__main__":
