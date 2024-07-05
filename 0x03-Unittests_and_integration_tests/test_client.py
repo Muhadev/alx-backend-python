71391cd49fa71d7ac50f996bfb555aa3c202c224
Muhadev <muhammedfayemi@gmail.com>
2024-07-05 23:20:18 +0100

    test_utils.py is added

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -44,7 +44,9 @@ class TestGithubOrgClient(unittest.TestCase):
         ({"license": {"key": "my_license"}}, "my_license", True),
         ({"license": {"key": "other_license"}}, "my_license", False)
     ])
-    def test_has_license(self, repo: dict, license_key: str, expected: bool) -> None:
+    def test_has_license(
+        self, repo: dict, license_key: str, expected: bool
+    ) -> None:
         """
         Test the has_license method.
         """
